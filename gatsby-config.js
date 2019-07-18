90038a60a0897d5e1430fc068826147623135b97
Jeff Au <jeffau13@gmail.com>
2019-07-18 16:09:55 -0400

    cloned into gatsby-starter-prologue

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,25 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
+const config = require('./config');
 
 module.exports = {
+  pathPrefix: config.pathPrefix,
+  siteMetadata: {
+    title: config.siteTitle,
+  },
   plugins: [
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
+    'gatsby-plugin-react-helmet',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: `gatsby-plugin-manifest`,
       options: {
-        name: "project",
-        path: "./data",
+        name: config.manifestName,
+        short_name: config.manifestShortName,
+        start_url: config.pathPrefix || config.manifestStartUrl,
+        background_color: config.manifestBackgroundColor,
+        theme_color: config.manifestThemeColor,
+        display: config.manifestDisplay,
+        icon: config.manifestIcon, // This path is relative to the root of the site.
       },
     },
-    "gatsby-transformer-json",
+    'gatsby-plugin-sass',
+    'gatsby-plugin-offline',
   ],
-}
+};
