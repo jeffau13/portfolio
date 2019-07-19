67cb0d8f5a8fe030ed1d10027a3d43b5d47e1684
Jeff Au <jeffau13@gmail.com>
2019-07-18 21:04:39 -0400

    added scroll behavior to header in nav

--- src/components/Sidebar/Header.js
+++ src/components/Sidebar/Header.js
@@ -1,13 +1,16 @@
 import React from "react"
 import { Link } from "gatsby"
-
+import Scroll from "../../components/Scroll"
 export default function Header({ title, heading }) {
   return (
     <div id="logo">
       <span className="image avatar48"></span>
-
-      <h1 id="title">{title}</h1>
-      <p>{heading}</p>
+      <Scroll type="id" element="main">
+        <Link to="#/">
+          <h1 id="title">{title}</h1>
+          <p>{heading}</p>
+        </Link>
+      </Scroll>
     </div>
   )
 }
