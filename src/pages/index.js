6ac5cc75cce77c06004232639a7f8891872a3a75
Jeff Au <jeffau13@gmail.com>
2019-08-11 18:57:37 -0400

    rearranged sections

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,9 +12,9 @@ import {
 
 const sections = [
   { id: 'top', name: 'Intro', icon: 'fa-home' },
-  { id: 'about', name: 'About Me', icon: 'fa-user' },
   { id: 'projects', name: 'Projects', icon: 'fa-th' },
-  { id: 'education', name: 'Education', icon: 'fa-graduation-cap' },
+  { id: 'about', name: 'About Me', icon: 'fa-user' },
+  // { id: 'education', name: 'Education', icon: 'fa-graduation-cap' },
   { id: 'contact', name: 'Contact', icon: 'fa-envelope' }
 ];
 
@@ -22,9 +22,9 @@ const IndexPage = () => (
   <Layout>
     <SideBar sections={sections} />
     <Intro />
-    <About />
     <Projects />
-    <Education />
+    <About />
+    {/* <Education /> */}
     <Contact />
   </Layout>
 );
