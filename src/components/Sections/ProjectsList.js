c4ff6f06b886c9209d1086ea2e907ece475d4111
Jeff Au <jeffau13@gmail.com>
2019-08-11 15:38:24 -0400

    fixed invalid key prop

--- src/components/Sections/ProjectsList.js
+++ src/components/Sections/ProjectsList.js
@@ -1,66 +1,61 @@
 import React, { Component } from 'react';
-import { StaticQuery, graphql } from "gatsby"
+import { StaticQuery, graphql } from 'gatsby';
 
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
 import ProjectCard from './ProjectCard';
 
 const getProjects = graphql`
-{
-  projects:
-    allContentfulPortfolioProjects{
-    edges{
-      node{
-        id
-        title
-        description
-        demoUrl
-        repoUrl
-        tags
-        order
-				screenshot{
-          fluid {
-            src
+  {
+    projects: allContentfulPortfolioProjects {
+      edges {
+        node {
+          id
+          title
+          description
+          demoUrl
+          repoUrl
+          tags
+          order
+          screenshot {
+            fluid {
+              src
+            }
           }
         }
       }
     }
   }
-}`
-
+`;
 
 const ProjectsList = () => {
   return (
-    <StaticQuery query={getProjects}
-     render =  { data=>{
-       
-       return(
-     
-         <Container>
-          <div>
-            <Grid
-              container
-              spacing={10}
-              alignItems="top"
-              style={{ padding: 2 }}
-            >
-              {data.projects.edges.map(item => {
-                return (
-                  <Grid key={item.id} item xs={12} sm={12} lg={6} xl={6}>
-                    <ProjectCard project={item} />
-                  </Grid>
-                );
-              })}
-            </Grid>
-          </div>
-        </Container>      
-       )}
-      
-      }
-  />
-  ) 
-}
+    <StaticQuery
+      query={getProjects}
+      render={data => {
+        return (
+          <Container>
+            <div>
+              <Grid
+                container
+                spacing={10}
+                alignItems="top"
+                style={{ padding: 2 }}
+              >
+                {data.projects.edges.map(item => {
+                  return (
+                    <Grid key={item.node.id} item xs={12} sm={12} lg={6} xl={6}>
+                      <ProjectCard project={item} />
+                    </Grid>
+                  );
+                })}
+              </Grid>
+            </div>
+          </Container>
+        );
+      }}
+    />
+  );
+};
 
-export default ProjectsList
-
+export default ProjectsList;
