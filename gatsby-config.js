88cf88e11740b7ef5ed8a92831b289eaa9aab5e7
Jeff Au <jeffau13@gmail.com>
2019-08-20 17:59:03 -0400

    removed unused options

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,20 +34,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS,
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ['/preview/**', '/do-not-track/me/too/'],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: 'jeffau.dev'
+        trackingId: process.env.GOOGLE_ANALYTICS
       }
     },
     'gatsby-plugin-sass',
